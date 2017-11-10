c684a94771fc796bd96372bb6d766a177c1d7902
colin93 <33459498+colin93@users.noreply.github.com>
2017-11-10 22:18:40 +0000

    Update poly.h

--- poly.h
+++ poly.h
@@ -5,23 +5,21 @@
 //1) enumerator for errors
 //2) All other function declerations
 
+
 typedef struct n {
-  int power;
-  double coeffecient
-  struct n *next; 
-} polyFactor;
+  int power; //Power of polynomial
+  double coeffecient //Coeffecient of polynomial
+  struct n *next; //Pointer to next element of polynomial
+} polyElement; //Name for creating an element of the polynomial
 
 typedef struct {
-  polyFactor *head;
-  polyFactor *current;
+  polyElement *head; //Creates a pointer to head of linked list
+  polyElement *current; //Creates a pointer to current polynomial element you are at
 } llist;
 
 llist polyMultiply(llist *poly,double mult);
 llist polyDivide(llist *poly,double divide);
 llist polyNormalise(llist *poly);
-
-
-
 
 
 #endif

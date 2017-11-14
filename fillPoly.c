74008788b6edd08caab2463f12dfb0f4964ddbfc
colin93 <33459498+colin93@users.noreply.github.com>
2017-11-14 21:11:55 +0000

    Update fillPoly.c

--- fillPoly.c
+++ fillPoly.c
@@ -3,24 +3,27 @@
 
 void fillPoly (llist *poly){
 
-	poly->current=poly->head->next;
-	int i;
-	while (poly->current !=NULL){
-		while (poly->current->next!=NULL){
+	poly->current=poly->head->next; //Go to first element
+	int i; //counter
+	while (poly->current !=NULL){ //While not at tail
+		while (poly->current->next!=NULL){ //While not at last element
 			for(i=(poly->current->power)-1;i>(poly->current->next->power);i--){
+				//fill in powers between two elements with coeffecient 0
 				insertElement(i,0,poly);
 				poly->current=poly->current->next;
 				}
-			poly->current=poly->current->next;
-		if (poly->current->power!=0){
+			poly->current=poly->current->next; //move to next element
+		if (poly->current->power!=0){ 
+			//goes here if at last element and its order is higher than 0
 			for(i=(poly->current->power)-1;i>=0;i--){
-				insertElement(i,0,poly);
+				//fills in values down to and including x^0 with coeffecient 0
+				insertElement(i,0,poly); 
 				poly->current=poly->current->next;
 				}
-			poly->current=poly->current->next;
+			poly->current=poly->current->next; //moves to tail
 			}
 		else 
-			poly->current=poly->current->next;
+			poly->current=poly->current->next; //moves to tail
 		}
 
 }

f72533652e1fb2ac30c4330697f6b69e9b9af8af
colin93 <33459498+colin93@users.noreply.github.com>
2017-11-14 20:52:35 +0000

    Update fillPoly.c

--- fillPoly.c
+++ fillPoly.c
@@ -6,31 +6,21 @@ void fillPoly (llist *poly){
 	poly->current=poly->head->next;
 	int i;
 	while (poly->current !=NULL){
-		if (poly->current->next!=NULL){
-			while (poly->current->next!=NULL){
-				for(i=(poly->current->power)-1;i>(poly->current->next->power);i--){
-					insertElement(i,0,poly);
-					poly->current=poly->current->next;
-					}
+		while (poly->current->next!=NULL){
+			for(i=(poly->current->power)-1;i>(poly->current->next->power);i--){
+				insertElement(i,0,poly);
 				poly->current=poly->current->next;
-			if (poly->current->power!=0)
-				for(i=(poly->current->power)-1;i>=0;i--){
-					insertElement(i,0,poly);
-					poly->current=poly->current->next;
-					}
-				poly->current=poly->current->next;
-			}		
-		}
-		else if((poly->current->next == NULL) && (poly->current->power !=0)){
+				}
+			poly->current=poly->current->next;
+		if (poly->current->power!=0){
 			for(i=(poly->current->power)-1;i>=0;i--){
 				insertElement(i,0,poly);
 				poly->current=poly->current->next;
 				}
 			poly->current=poly->current->next;
-		}
-		else
+			}
+		else 
 			poly->current=poly->current->next;
-	}
-	
+		}
 
 }

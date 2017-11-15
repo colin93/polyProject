2e06aaa027b551918d9cf88a803dd8a54bac1111
colin93 <33459498+colin93@users.noreply.github.com>
2017-11-15 15:02:48 +0000

    Update fillPoly.c

--- fillPoly.c
+++ fillPoly.c
@@ -7,23 +7,23 @@ void fillPoly (polynomial *poly){
 	int i; //counter
 	while (poly->structure->current !=NULL){ //While not at tail
 		while (poly->structure->current->next!=NULL){ //While not at last element
-			for(i=(poly->structure->current->power)-1;i>(poly->structure->current->next->power);i--){
+			for(i=(getPower(poly))-1;i>(poly->structure->current->next->power);i--){
 				//fill in powers between two elements with coeffecient 0
 				insertElement(i,0,poly);
-				poly->structure->current=poly->structure->current->next;
+				nextElement(poly);
 				}
-			poly->structure->current=poly->structure->current->next; //move to next element
-		if (poly->structure->current->element->power!=0){ 
+			nextElement(poly); //move to next element
+		if (getPower(poly)!=0){ 
 			//goes here if at last element and its order is higher than 0
-			for(i=(poly->structure->current->element->power)-1;i>=0;i--){
+			for(i=(getPower(poly))-1;i>=0;i--){
 				//fills in values down to and including x^0 with coeffecient 0
 				insertElement(i,0,poly); 
-				poly->structure->current=poly->structure->current->next;
+				nextElement(poly);
 				}
-			poly->structure->current=poly->structure->current->next; //moves to tail
+			nextElement(poly); //moves to tail
 			}
 		else 
-			poly->structure->current=poly->structure->current->next; //moves to tail
+			nextElement(poly); //moves to tail
 		}
 
 }

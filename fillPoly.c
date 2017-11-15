b28ed28c473f85978ca438e776708115310baddf
colin93 <33459498+colin93@users.noreply.github.com>
2017-11-15 14:27:19 +0000

    Update fillPoly.c

--- fillPoly.c
+++ fillPoly.c
@@ -1,29 +1,29 @@
 #include "poly.h"
 #include <stdlib.h>
 
-void fillPoly (llist *poly){
+void fillPoly (polynomial *poly){
 
-	poly->current=poly->head->next; //Go to first element
+	poly->structure->current=poly->structure->head->next; //Go to first element
 	int i; //counter
-	while (poly->current !=NULL){ //While not at tail
-		while (poly->current->next!=NULL){ //While not at last element
-			for(i=(poly->current->power)-1;i>(poly->current->next->power);i--){
+	while (poly->structure->current !=NULL){ //While not at tail
+		while (poly->structure->current->next!=NULL){ //While not at last element
+			for(i=(poly->structure->current->power)-1;i>(poly->structure->current->next->power);i--){
 				//fill in powers between two elements with coeffecient 0
 				insertElement(i,0,poly);
-				poly->current=poly->current->next;
+				poly->structure->current=poly->structure->current->next;
 				}
-			poly->current=poly->current->next; //move to next element
-		if (poly->current->power!=0){ 
+			poly->structure->current=poly->structure->current->next; //move to next element
+		if (poly->structure->current->element->power!=0){ 
 			//goes here if at last element and its order is higher than 0
-			for(i=(poly->current->power)-1;i>=0;i--){
+			for(i=(poly->structure->current->element->power)-1;i>=0;i--){
 				//fills in values down to and including x^0 with coeffecient 0
 				insertElement(i,0,poly); 
-				poly->current=poly->current->next;
+				poly->structure->current=poly->structure->current->next;
 				}
-			poly->current=poly->current->next; //moves to tail
+			poly->structure->current=poly->structure->current->next; //moves to tail
 			}
 		else 
-			poly->current=poly->current->next; //moves to tail
+			poly->structure->current=poly->structure->current->next; //moves to tail
 		}
 
 }

bdf337f24279b354dd012e13cd8c2e3ce96d3f4f
colin93 <33459498+colin93@users.noreply.github.com>
2017-11-15 15:18:55 +0000

    Update polyCopy.c

--- polyCopy.c
+++ polyCopy.c
@@ -8,20 +8,16 @@ polynomial *copyPoly(polynomial *polyIn){
   polynomial *polyCopy; //Creates linked list for copying
   polyCopy = (polynomial*)malloc(sizeof(polynomial)); //Allocates memory for polyCopy
   
-  polyIn->structure->current = polyIn->structure->head->next; //Sets node of poly to be copied to first element in list
-  polyCopy->structure->current = polyCopy->structure->head; //Sets node of list for copying to head
+  goToHead(polyIn);//Sets node of poly to be copied to first element in list
+  nextElement(polyIn);
+  goToHead(polyCopy); //Sets node of list for copying to head
   
   while (polyIn->structure->current != NULL) //Continues if not at tail of input llist
   {
-    llnode *elementCopy; //Creates node for copying elements
-    elementCopy = (*llnode)malloc(sizeof(llnode)); //allocates memory for new node
+    insertElement(getPower(polyIn),getCoeffecient(polyIn),polyCopy);
     
-    elementCopy->element->coeffecient = getCoeffecient(polyIn); //copies values for coeffecient of input poly
-    elementCopy->element->power = getPower(polyIn);   //Copies values of powers for input polynomial
-    elementCopy->next = polyCopy->structure->current->next;  //Sets next pointer of new node to tail of copy polynomial
-    polyCopy->structure->current->next = elementCopy; //Sets pointer for next node in polyCopy to new node
     nextElement(polyCopy); //Sets current in polyCopy to new node
-    nestElement(polyIn); //Moves to next element in input polynomial
+    nextElement(polyIn); //Moves to next element in input polynomial
   }
   
   return polyCopy; //Returns the copied polynomial

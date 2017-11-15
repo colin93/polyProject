44cfe3dd0cceb5e167192288697cf23db54306da
colin93 <33459498+colin93@users.noreply.github.com>
2017-11-15 22:45:36 +0000

    Update addPoly.c

--- addPoly.c
+++ addPoly.c
@@ -5,52 +5,51 @@
 
 polynomial *polyAdd(polynomial *poly1,polynomial*poly2)
 {
-polynomial *result;//This is creating Polynominal
-  
-//Check if both poly llist is empty
-if (poly1->head->next == NULL || poly2->head->next == NULL){
-printf("Both supplied polynomials are empty");
-result = NULL;
-return result;
-//return any of supplied polinomials as both are empty
-}
-/*//Check if only first poly llist is empty
-else if (poly1->head->next == NULL && poly2->head->next != NULL){
-printf("First supplied polynomial is empty");
-return poly2; //return the second supplied polinomian as first is empty
-}
-//Check if only second poly llist is empty
-else if (poly1->head->next != NULL && poly2->head->next == NULL){
-printf("Second supplied polynomial is empty");
-return poly1; //return the first supplied polinomian as second is empty
-}*/
-else{
-int polyPowers1 = polyOrder(poly1);
-int polyPowers2 = polyOrder(poly2);
-if(polyPower1 >= polyPower2){
-result = copyPoly1(poly1)
-while(result->structure->current->element->power > poly2->structure->current->element->power)
-result->stucture->currrent->element = result->structure->current->element->next;
-while(result->structure->current->element != NULL)
-result->structure->current->element->coeffecient = result->strucutre->current->element->coeffecient + poly2->structure->current-
->element->coeffecient;//change to -
-poly2->structure->currrent->element = poly2->structure->current->element->next
-result->structure->currrent->element = result->structure->current->element->next;
-printf(" %lf is coeficient of first poly and %lf is the coeficient of second poly", poly1->structure->current-
->element->coeffecient , poly2->structure->current->element->coeffecient);
+  polynomial *result;//This is creating Polynominal
 
-}else{result = copyPoly1(poly2)
+  //Check if both poly llist is empty
+  if (poly1->head->next == NULL || poly2->head->next == NULL){
+  printf("Both supplied polynomials are empty");
+  result = NULL;
+  return result;
+  //return any of supplied polinomials as both are empty
+  }
+  /*//Check if only first poly llist is empty
+  else if (poly1->head->next == NULL && poly2->head->next != NULL){
+  printf("First supplied polynomial is empty");
+  return poly2; //return the second supplied polinomian as first is empty
+  }
+  //Check if only second poly llist is empty
+  else if (poly1->head->next != NULL && poly2->head->next == NULL){
+  printf("Second supplied polynomial is empty");
+  return poly1; //return the first supplied polinomian as second is empty
+  }*/
+  else{
+    int polyPowers1 = polyOrder(poly1);
+    int polyPowers2 = polyOrder(poly2);
+    if(polyPower1 >= polyPower2){
+      result = copyPoly1(poly1)
+      while(getPower(result) > polyPowers2)
+        nextElement(result);
+      while(result->structure->current->element != NULL){
+        setCoeffecient(result,getCoeffecient(result)+getCoeffecient(poly2));
+        nextElement(result);
+        nextElement(poly2);
+      }
+    }
+    else{
+      result = copyPoly1(poly2)
 
-while(result->sturcture->current->element->power > poly1->structure->current->element->power)
-result->structure->currrent->element = result->structure->current->element->next;
-while(result->structure->current != NULL)
-result->structure->current->element->coeffecient = result->structure->current->element->coeffecient + poly1->structure->current-
->element->coeffecient;
-poly1->structure->currrent = poly1->structure->current->element->next
-result->structure->currrent-element = result->structure->current->element->next;
-}
+      while(getPower(result) > polyPowers1)
+        nextElement(result);
+      while(result->structure->current != NULL){
+        setCoeffecient(result,getCoeffecient(poly1)+getCoeffecient(result));
+        nextElement(poly1);
+        nextElement(result);
+      }
+    }
 
-}
+  }
   
   return result; //returns value of poly after multiplication
 }

f400c5a8c373be32202385708fcc058bb5f69b4b
colin93 <33459498+colin93@users.noreply.github.com>
2017-11-20 17:38:54 +0000

    Update polyMain.c

--- polyMain.c
+++ polyMain.c
@@ -5,116 +5,53 @@
 //Will need to update all functions here to reflect all the functions that will be create
 
 int main(){
-
-  int option=9; //choice for switch
-  polynomial *poly1 = createPoly(); //polynomial for performing various functions
-
-  while (option!=8){
-    //table of operations
-    printf("\n\nPick an operation \n");
-    printf("\t1)Print poly\n");
-    printf("\t2)Print order of poly\n");
-    printf("\t3)Add two polynomials\n");
-    printf("\t4)Subtract two polynomials\n");
-    printf("\t5)Multiply polynomial by a number\n");
-    printf("\t6)Divide polynomial by a number\n");
-    printf("\t7)Normalise polynomial\n");
-    printf("\t8)Delete polynomial and exit\n");
-    scanf("%d",&option);
-
-    switch (option) {
-
-    case 1: //Print out polynomial
-      printPoly(poly1);
-      break;
-      
-    case 2: //Print order of polynomial
-      printf("%d",polyOrder(poly1));
-      break;
-      
-    case 3:
-      //Creates second polynomial and adds it to first
-      //Prints out result then deletes resultant and second polynomial
-      printf("Create a second polynomial\n");
-      polynomial *poly2 = createPoly();
-      polynomial *poly3 = polyAdd(poly1,poly2);
-      printPoly(poly1);
-      printf("+");
-      printPoly(poly2);
-      printf("=");
-      printPoly(poly3);
-      deletePoly(poly2);
-      deletePoly(poly3);
-      break;
-      
-    case 4:
-      //Creates second polynomial and subtracts it from first
-      //Prints result and deletes resultant and second polynomial
-      printf("Create a second polynomial\n");
-      polynomial *poly2 = createPoly();
-      polynomial *poly3 = polySub(poly1,poly2);
-      printPoly(poly1);
-      printf("-");
-      printPoly(poly2);
-      printf("=");
-      printPoly(poly3);
-      deletePoly(poly2);
-      deletePoly(poly3);
-      break;
-      
-    case 5:
-      //takes in value and multiplies polynomial by that value
-      //Prints result and deletes resultant polynomial
-      double mult;
-      printf("Please enter a value for multiplication");
-      scanf("%lf",&mult);
-      polynomial *poly2 = polyMultiply(poly1,mult);
-      printPoly(poly1);
-      printf("multiplied by %f is",mult);
-      printPoly(poly2);
-      deletePoly(poly2);
-      break;
-      
-    case 6:
-      //takes in value and divides polynomial by that value
-      //Prints result and deletes resultant polynomial
-      double div;
-      printf("Please enter a value for division");
-      scanf("%lf",&mult);
-      polynomial *poly2 = polyDivide(poly1,div);
-      printPoly(poly1);
-      printf("divided by %f is",div);
-      printPoly(poly2);
-      deletePoly(poly2);
-      break;
-      
-    case 7:
-      //Normalise and print poly
-      //Then deletes normalised polynomial
-      polynomial *poly2 = polyNormalise(poly1);
-      printPoly(poly1);
-      printf("\nNormalised is");
-      printPoly(poly2);
-      deletePoly(poly2);
-      break;
-      
-    case 8:
-      //Deletes original polynomial and exits program
-      polyDelete(poly1);
-      printf("Exiting program\n");
-      break;
-    
-    default:
-      printf("Invalid input. Exiting program\n");
-      option = 8;
-      polyDelete(poly1);
-      break;
-	      
+ 
+  printf("Creating poly1:\n");
+  polynomial *poly1 = createPoly();
+  insertElement (,,poly1);
+  insertElement (,,poly1);
+  insertElement (,,poly1);
+  insertElement (,,poly1);
+  printf("Attempt to input negative power\n");
+  insertElement(-1,2,poly1);
+  fillPoly(poly1);
+  printf("poly1 has order %d",polyOrder(poly1));
 	
-      
-    }
-    
-
-  }
+  printf("Creating poly2:\n");
+  polynomial *poly2 = createPoly();
+  insertElement (,,poly2);
+  insertElement (,,poly2);
+  insertElement (,,poly2);
+  insertElement (,,poly2);
+  fillPoly(poly2);
+	
+  printf("Value of poly1 is:\n");
+  printPoly(poly1);
+  printf("\nValue of poly2 is :\n");
+  printPoly(poly1);
+	
+  printf("Adding poly2 to poly1 results in:");
+  printPoly(poly1);
+	
+  printf("Subtracting poly2 from this results in:");
+  printPoly(poly1);
+	
+  printf("Multiplying by 5 results in:\n");
+  polyMultiply(poly1,5.0);
+  printPoly(poly1);
+	
+  printf("Attempt to divide by 0")
+  polyDivide(poly1,0);
+	
+  printf("Dividing poly1 by 5 results in\n");
+  polyDivide(poly1,5.0);
+  
+  printf("Normalising results in:\n")
+  polyNormalise(poly1);
+  printf(poly1);
+	 	
+	
+	
+	
   return 0; //Exit main
 }

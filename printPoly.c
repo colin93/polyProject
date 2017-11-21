0090d375c664f0850bd835c2abb49cfbbed0f3c4
colin93 <33459498+colin93@users.noreply.github.com>
2017-11-21 16:26:20 +0000

    Create printPoly.c

---
+++ printPoly.c
@@ -0,0 +1,12 @@
+#include "poly.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void printPoly(polynomial *poly){
+
+  goToHead(poly)
+  while(nextElement(poly)==ok){
+    if(getCoeffecient(poly)!=0)
+      printf("%.2fx^%d",getCoeffecient(poly),getPower(poly));
+  }
+}

e0ba8869263216fe85622e40d6fc659bfac1559e
colin93 <33459498+colin93@users.noreply.github.com>
2017-11-21 19:30:00 +0000

    Update printPoly.c

--- printPoly.c
+++ printPoly.c
@@ -5,8 +5,27 @@
 void printPoly(polynomial *poly){
 
   goToHead(poly);
-  while(nextElement(poly)==okay){
-    if(getCoeffecient(poly)!=0)
-      printf("%.2fx^%d",getCoeffecient(poly),getPower(poly));
+  nextElement(poly);
+  if(getPower(poly)!=0)
+    printf("%.2fx^%d",getCoeffecient(poly),getPower(poly));
+  else
+    printf("%.2f",getCoeffecient(poly));
+  nextElement(poly);
+  while(poly->structure->current->next!=NULL){
+    if(getCoeffecient(poly)>0){
+      printf(" + %.2fx^%d",getCoeffecient(poly),getPower(poly));
+      nextElement(poly);
+    }
+    else if(getCoeffecient(poly)<0){
+      printf(" - %.2fx^%d",-getCoeffecient(poly),getPower(poly));
+      nextElement(poly);
+    }
+    else
+      nextElement(poly);
+       
   }
+  if (getCoeffecient(poly)>0)
+      printf(" + %.2f",getCoeffecient(poly));
+  else if(getCoeffecient(poly)<0)
+    printf(" - %.2f" ,-getCoeffecient(poly));
 }

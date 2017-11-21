ab21851ebe79b8d2d0a7e2f5c5545c1546539d91
colin93 <33459498+colin93@users.noreply.github.com>
2017-11-21 12:34:53 +0000

    Update fillPoly.c

--- fillPoly.c
+++ fillPoly.c
@@ -3,27 +3,23 @@
 
 void fillPoly (polynomial *poly){
 
-	poly->structure->current=poly->structure->head->next; //Go to first element
-	int i; //counter
-	while (poly->structure->current !=NULL){ //While not at tail
-		while (poly->structure->current->next!=NULL){ //While not at last element
-			for(i=(getPower(poly))-1;i>(poly->structure->current->next->power);i--){
-				//fill in powers between two elements with coeffecient 0
-				insertElement(i,0,poly);
-				nextElement(poly);
-				}
-			nextElement(poly); //move to next element
-		if (getPower(poly)!=0){ 
-			//goes here if at last element and its order is higher than 0
-			for(i=(getPower(poly))-1;i>=0;i--){
-				//fills in values down to and including x^0 with coeffecient 0
-				insertElement(i,0,poly); 
-				nextElement(poly);
-				}
-			nextElement(poly); //moves to tail
-			}
-		else 
-			nextElement(poly); //moves to tail
-		}
+   poly->structure->current=poly->structure->head->next; //Go to first element
+   int i; //counter
+   while (poly->structure->current->next!=NULL){ //While not at last element
+      for(i=(getPower(poly))-1;i>(poly->structure->current->next->power);i--){
+	 //fill in powers between two elements with coeffecient 0
+ 	 insertElement(i,0,poly);
+	 nextElement(poly);//moves to inserted node
+      }
+      nextElement(poly); //move to next element that was already defined
+   }
+   if (getPower(poly)!=0){ 
+      //goes here if at last element and its order is higher than 0
+      for(i=(getPower(poly))-1;i>=0;i--){
+         //fills in values down to and including x^0 with coeffecient 0
+         insertElement(i,0,poly); 
+         nextElement(poly); //moves to inserted element
+      }
+   }
 
 }

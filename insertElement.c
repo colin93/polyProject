a28982fcbc88c7fcf2ed5585afd784d0e9b4d728
colin93 <33459498+colin93@users.noreply.github.com>
2017-11-21 12:42:48 +0000

    Update insertElement.c

--- insertElement.c
+++ insertElement.c
@@ -1,19 +1,27 @@
 #include "poly.h"
 #inlcude <stdlib.h>
 
-void insertElement(int power, double coeffecient,polynomial *poly){
-
-  llnode *newelement; //create new element
-
-  newelement = (llnode *) malloc(sizeof(llnode)); //allocate memory for element
-
-  if (newelement !=NULL) {
-    newelement->element->power = power; //set power of new element to input power
-    newelement->element->coeffecient = coeffecient; //set coeffecient of element to input coeffecient
-
-    newelement->next = poly->structure->current->next //set next in new element to next pointer in poly
-    poly->structure->current->next = newelement; //link new elemnet into poly
-  }
+eleError insertElement(int power, double coeffecient,polynomial *poly){
   
+  eleError result = ok;
+  
+  if(power < 0)
+    result = negPower;
+  else{
 
+    llnode *newelement; //create new element
+
+    newelement = (llnode *) malloc(sizeof(llnode)); //allocate memory for element
+
+    if (newelement !=NULL) {
+      newelement->element->power = power; //set power of new element to input power
+      newelement->element->coeffecient = coeffecient; //set coeffecient of element to input coeffecient
+
+      newelement->next = poly->structure->current->next //set next in new element to next pointer in poly
+      poly->structure->current->next = newelement; //link new elemnet into poly
+    }
+    else 
+      result = noMemory;
+  }
+  return result;
 }
